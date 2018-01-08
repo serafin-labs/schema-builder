3b1a61825b9ec93c09fa9ed8e4686e2a15e67bef
Nicolas Degardin <degardin.n@gmail.com>
2018-01-08 16:38:00 -0500

    Gulp development task

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var gulpTasks = require('@serafin/gulp-tasks');
+
+gulp.task('default', ['dev']);
+gulp.task('dev', ['watch', 'watch-build-done', 'test']);
+gulp.task('watch', ['watch-typescript']);
+gulp.task('build', ['build-typescript']);
+gulp.task('build-done', ['test']);
+
+gulpTasks.typescript(gulp, __dirname + '/src', __dirname + '/src/tsconfig.json', __dirname + '/lib', __dirname + '/lib/typings');
+gulpTasks.utils(gulp, __dirname + '/lib');
+gulpTasks.runner(gulp, null, __dirname + '/lib/build.txt', __dirname + '/lib/pid', true);
+gulpTasks.test(gulp, __dirname + '/lib/**/test/*.js', __dirname + '/lib/coverage');
