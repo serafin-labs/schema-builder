602f836005ca1942e24800d2577664ec6f80f097
Sebastien De Saint Florent <sebastien.dsf@gmail.com>
2022-01-07 14:21:04 -0500

    Fix debug configuration, fix webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -3,59 +3,57 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 const nodeExternals = require("webpack-node-externals")
 const WebpackShellPlugin = require("webpack-shell-plugin-next")
 
-// detect production/development mode
-console.log(`Building for ${process.env.NODE_ENV}`)
-const isProduction = process.env.NODE_ENV === "production"
+module.exports = function (env, argv) {
+    const isProduction = !(argv.mode === "development")
+    console.log(`Building mode:${argv.mode || "production"}`)
 
-var configuration = {
-    entry: {
-        index: "./src/index.ts",
-        test: "./src/test/index.ts",
-    },
-    target: "node",
-    externals: [nodeExternals()],
-    devtool: "nosources-source-map",
-    watch: !isProduction,
-    watchOptions: {
-        poll: 1000,
-        ignored: /node_modules|lib/,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
+    return {
+        entry: {
+            index: "./src/index.ts",
+        },
+        target: "node",
+        externals: [nodeExternals()],
+        devtool: "nosources-source-map",
+        watch: !isProduction,
+        watchOptions: {
+            poll: 1000,
+            ignored: /node_modules|lib/,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        optimization: {
+            minimize: false,
+        },
+        resolve: {
+            extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+        },
+        output: {
+            filename: "[name].js",
+            libraryTarget: "commonjs",
+            devtoolModuleFilenameTemplate: (info) => (info.resourcePath.startsWith("./src") ? `.${info.resourcePath}` : info.absoluteResourcePath),
+            path: path.resolve(__dirname, "lib"),
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            ...(isProduction
+                ? []
+                : [
+                      new WebpackShellPlugin({
+                          swallowError: true,
+                          onDoneWatch: {
+                              scripts: ["npm test"],
+                              blocking: false,
+                              parallel: true,
+                          },
+                      }),
+                  ]),
         ],
-    },
-    optimization: {
-        minimize: false,
-    },
-    resolve: {
-        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
-    },
-    output: {
-        filename: "[name].js",
-        libraryTarget: "commonjs",
-        devtoolModuleFilenameTemplate: (info) => (info.resourcePath.startsWith("./src") ? `.${info.resourcePath}` : info.absoluteResourcePath),
-        path: path.resolve(__dirname, "lib"),
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        ...(isProduction
-            ? []
-            : [
-                  new WebpackShellPlugin({
-                      swallowError: true,
-                      onDoneWatch: {
-                          scripts: ["npm test"],
-                          blocking: false,
-                          parallel: true,
-                      },
-                  }),
-              ]),
-    ],
+    }
 }
-
-module.exports = configuration
